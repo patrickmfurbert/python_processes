bfce778986d6108c1bdffb3d0cdabec3499f5187
Kali <kali@kali>
2021-07-26 07:18:48 -0400

    threads.py creates 4 threads that sleep for 25 seconds

--- threads.py
+++ threads.py
@@ -1,17 +1,18 @@
 # Author: Patrick Furbert
 # Date: 7/26/2021
+# This program spawns four threads that go to sleep for 25 seconds
 
 import time
 import concurrent.futures
 
 def function_sleep():
-    time.sleep(20)
+    time.sleep(25)
 
 def main():
     
-    # with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-    #     executor.map(function_sleep)
-    function_sleep()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        for x in range(4):
+            executor.submit(function_sleep)
 
 if __name__ == "__main__":
     main()
