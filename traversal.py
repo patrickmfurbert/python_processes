d187f5371d1b7703c3e93ce96692ebdbaa43e704
Kali <kali@kali>
2021-07-27 21:36:11 -0400

    traversal.py created to get at shared objects of a process

---
+++ traversal.py
@@ -0,0 +1,31 @@
+# Author: Patrick Furbert
+# Date: July 27, 2021
+
+import os
+import sys
+
+maps = []
+shared_objects = set()
+
+
+with open(f'/proc/{sys.argv[1]}/maps') as file:
+    maps = [line.rstrip() for line in file]
+
+for line in maps:
+    if '.so' in line.split('/')[-1]:
+        shared_objects.add(line.split('/')[-1])
+
+
+for object in shared_objects:
+    print(object)
+
+
+
+# directory_tree = []
+
+# for root, dir, files in os.walk('/proc'):
+#     directory_tree.append((root, dir, files))
+
+
+# for entry in directory_tree:
+#     print(entry)
